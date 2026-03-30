ad8f482dc9457165846583c86c9607af890b964b
박천규 <pckyu01@naver.com>
2026-03-30 17:23:52 +0900

    cleanup

--- src/main/java/org/example/Profiler.java
+++ src/main/java/org/example/Profiler.java
@@ -3,41 +3,37 @@ package org.example;
 import java.util.ArrayList;
 
 public class Profiler {
-    // TODO : Profiler 구현
     private ArrayList<Integer> _data;
     private SortType _sortType = SortType.BUBBLE_SORT;
     private SortServiceLib _sortLib = new BubbleSort();
     private Integer _swap_count = 0;
 
     public void setData(ArrayList<Integer> data) {
-        this._data = data;
+        _data = data;
     }
 
     public void setLib(SortType type) {
-        this._sortType = type;
+        _sortType = type;
         switch (type) {
             case BUBBLE_SORT:
-                this._sortLib = new BubbleSort();
+                _sortLib = new BubbleSort();
                 break;
             case INSERTION_SORT:
-                this._sortLib = new InsertionSort();
+                _sortLib = new InsertionSort();
                 break;
             case SELECTION_SORT:
-                this._sortLib = new SelectionSort();
-                break;
-            default:
-                this._sortLib = null;
+                _sortLib = new SelectionSort();
                 break;
         }
     }
 
     public void runLib() {
-        this._swap_count = this._sortLib.sort(this._data);
+        _swap_count = _sortLib.sort(_data);
     }
 
     public void showResult() {
-        System.out.println(this._sortType + " 테스트");
-        System.out.println("정렬결과: " + this._data.toString());
-        System.out.println("swap횟수: " + this._swap_count);
+        System.out.println(_sortType + " 테스트");
+        System.out.println("정렬결과: " + _data.toString());
+        System.out.println("swap횟수: " + _swap_count);
     }
 }
